4699bba5760bf88d58c975b3c79c8159fdd487e3
andre.orvalho <andre.orvalho@gmail.com>
2023-03-21 10:36:52 +0000

    making transaction and decoding message

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
-var Web3 = require('web3');
-var web3 = new Web3('http://127.0.0.1:8545');
-web3.eth.getAccounts().then(accounts => {
-  account1 = accounts[0].toLowerCase();
+const Web3 = require('web3');
+const fs = require('fs');
+const web3 = new Web3('http://127.0.0.1:8545');
+const Web3Utils = require('web3-utils');
 
-  web3.eth.personal.unlockAccount(account1, "12345", 15000).then( unlockStatus => {
-    console.log("Account: " + account1 + "is unlocked: " + unlockStatus);
-    web3.eth.getBalance(account1).then(a => console.log(a));
-    // web3.eth.sendTransaction({ from: account1, to: '0xd871803ba61ed24c6dff76e5888fe83ca37aac74' , value: 1, gasPrice: 1000, data: '0x616e647265' },
-    // function(err, transactionHash) {
-    //  if (err) {
-    //   console.log("Dummy transaction error:", err);
-    //  }
-    //  console.log('hello');
-    //  console.log(transactionHash);
-    //  return;
-    // });
+const getNode1Adress = async () => {
+  const accounts = await web3.eth.getAccounts();
+  return accounts[0].toLowerCase();
+}
 
+const unlockAccount = async (password) => {
+  const node1Address = await getNode1Adress();
+  const unlockStatus = await web3.eth.personal.unlockAccount(node1Address, password, 15000)
+  if (!unlockStatus) {
+    throw new Error('Cannot unlock wallet');
+  }
+  return node1Address;
+}
 
- });
+const makeTransactionTo = async ({ receivingAddress, value = 10000, data = '0x616e647265'}) => {
+  const node1Address = await unlockAccount('12345')
+  web3.eth.getBalance(node1Address).then(balance => console.log("Balance on account of node1: " + balance));
+
+  const transactionDetails = await web3.eth.sendTransaction(
+    {
+      from: node1Address,
+      to:  receivingAddress,
+      value: value,
+      gasPrice: 1000,
+      data: data
+    }
+  );
+  console.log("Transaction hash: " + transactionDetails.transactionHash);
+  console.log("done");
+  return transactionDetails.transactionHash;
+}
+
+const getTransanction = async (transactionHash) => {
+  const transactionDetails = await web3.eth.getTransaction(transactionHash);
+  return transactionDetails;
+}
+
+console.log("transfers from node1 to node2")
+
+const files = fs.readdirSync(`${process.cwd()}/node2/keystore`);
+const data = JSON.parse(fs.readFileSync(`${process.cwd()}/node2/keystore/${files[0]}`));
+const node2Address = `0x${data.address}`;
+
+const message = Web3Utils.utf8ToHex("yo wassup");
+
+makeTransactionTo({ node2Address, data: message }).then(transactionHash => {
+  getTransanction(transactionHash).then(transactionDetails => {
+    console.log("Message on transaction: " + Web3Utils.hexToString(transactionDetails.input));
+  });
 });
-
